741de5afd63e694e870a2b66a25f8f078db8f069
ichirou2910 <ichiroukeita2910@gmail.com>
2025-02-05 00:30:40 +0700

    feat: firewall for wireguard

--- hosts/artemis/default.nix
+++ hosts/artemis/default.nix
@@ -27,6 +27,19 @@
   networking = {
     networkmanager.enable = true;
     hostName = "artemis";
+    firewall = {
+      logReversePathDrops = true;
+      extraCommands = ''
+        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
+        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
+      '';
+      extraStopCommands = ''
+        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
+        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
+      '';
+    };
+  };
+
   home-manager.users.ichirou.xdg.configFile = {
     "kdeconnect/config" = {
       source = pkgs.writeText "config" ''
