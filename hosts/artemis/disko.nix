9d0caea93cfae1ab86b2fc792201946c3ddf40fa
ichirou2910 <ichiroukeita2910@gmail.com>
2025-02-04 12:45:48 +0700

    feat(artemis): add disko

---
+++ hosts/artemis/disko.nix
@@ -0,0 +1,33 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        device = "ata-LITEON_L8H-128V2G_0025491029TD";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "512M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
