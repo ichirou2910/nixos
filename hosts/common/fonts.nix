f4df1f71c0fdea86396b1790aa3d965f49e4b557
ichirou2910 <ichiroukeita2910@gmail.com>
2025-02-04 15:36:34 +0700

    remove fontProfile

--- hosts/common/fonts.nix
+++ hosts/common/fonts.nix
@@ -14,16 +14,4 @@
       };
     };
   };
-
-  fontProfiles = {
-    enable = true;
-    monospace = {
-      family = "Fantasque Sans Mono Nerd Font";
-      package = pkgs.nerdfonts.override {fonts = ["FantasqueSansMono" "JetBrainsMono"];};
-    };
-    regular = {
-      family = "Noto";
-      package = pkgs.noto-fonts;
-    };
-  };
 }
