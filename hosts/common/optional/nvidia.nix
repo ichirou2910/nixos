63bacf91fe522d9b3531c06781c35cd8ff0b48c1
ichirou2910 <ichiroukeita2910@gmail.com>
2025-02-04 13:28:51 +0700

    fix: remove nvidia settings

--- hosts/common/optional/nvidia.nix
+++ hosts/common/optional/nvidia.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, ... }: {
   # Enable graphics driver
-  # hardware.graphics.enable = true;
+  hardware.graphics.enable = true;
 
   # Load "nvidia" driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"];
@@ -28,10 +28,6 @@
     # Only available from driver 515.43.04+
     # Currently "beta quality", so false is currently the recommended setting.
     open = false;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
