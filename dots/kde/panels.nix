81a529ae4f58c4c9bdebc5bcb744230e3609614a
ichirou2910 <ichiroukeita2910@gmail.com>
2025-02-10 14:59:04 +0700

    refactor(kde): cleanup panels

--- dots/kde/panels.nix
+++ dots/kde/panels.nix
@@ -1,94 +1,61 @@
 {
-  programs.plasma.panels = [
-    # main screen
-    {
-      screen = 0;
-      floating = true;
-      height = 45;
-      hiding = "none";
-      lengthMode = "fill";
-      location = "bottom";
-      widgets = [
-        {
-          kickoff = {
-            icon = "distributor-logo-nixos";
-          };
-        }
-        {
-          iconTasks = {
-            behavior = {
-              showTasks = {
-                onlyInCurrentScreen = true;
-                onlyInCurrentDesktop = true;
-                onlyInCurrentActivity = true;
-                onlyMinimized = false;
-              };
-            };
-            launchers = [ ];
-          };
-        }
-        "org.kde.plasma.marginsseparator"
-        "org.kde.plasma.systemtray"
-        {
-          digitalClock = {
-            date.format = { 
-              custom = "ddd, MMM d, yyyy";
+  programs.plasma.panels =
+    let
+      appLauncher = {
+        kickoff = {
+          icon = "nix-snowflake";
+        };
+      };
+
+      iconTasks = {
+        iconTasks = {
+          behavior = {
+            showTasks = {
+              onlyInCurrentScreen = true;
+              onlyInCurrentDesktop = true;
+              onlyInCurrentActivity = true;
+              onlyMinimized = false;
             };
           };
-        }
-      ];
-    }
-    # 2nd screen
-    {
-      screen = 1;
-      floating = true;
-      height = 45;
-      hiding = "none";
-      lengthMode = "fill";
-      location = "bottom";
-      widgets = [
-        {
-          kickoff = {
-            icon = "distributor-logo-nixos";
+          launchers = [ ];
+        };
+      };
+
+      cpu = "org.kde.plasma.systemmonitor.cpu";
+      memory = "org.kde.plasma.systemmonitor.memory";
+
+      margin = "org.kde.plasma.marginsseparator";
+
+      tray = "org.kde.plasma.systemtray";
+
+      clock = {
+        digitalClock = {
+          date.format = { 
+            custom = "ddd, MMM d, yyyy";
           };
-        }
-        {
-          iconTasks = {
-            behavior = {
-              showTasks = {
-                onlyInCurrentScreen = true;
-                onlyInCurrentDesktop = true;
-                onlyInCurrentActivity = true;
-                onlyMinimized = false;
-              };
-            };
-            launchers = [ ];
-          };
-        }
-        {
-          systemMonitor = {
-            title = "Total CPU Use";
-            showTitle = true;
-            showLegend = true;
-            displayStyle = "org.kde.ksysguard.piechart";
-            sensors = [
-              {
-                name = "cpu/all/usage";
-                color = "212,21,21";
-                label = "CPU %";
-              }
-            ];
-          };
-        }
-        "org.kde.plasma.marginsseparator"
-        {
-          digitalClock = {
-            date.format = { 
-              custom = "ddd, MMM d, yyyy";
-            };
-          };
-        }
-      ];
-    }
-  ];
+        };
+      };
+    in
+    [
+      # main screen
+      {
+        screen = 0;
+        floating = true;
+        height = 45;
+        hiding = "none";
+        lengthMode = "fill";
+        location = "bottom";
+        widgets = [
+          appLauncher
+          iconTasks
+          cpu
+          memory
+          margin
+          tray
+          clock
+        ];
+      }
+
+      # sub screen etc.
+    ];
 }
